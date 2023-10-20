43c949185e4a792773d48294c09bec1ca6714bc5
BigFriedChicken <127102395+BigFriedChicken@users.noreply.github.com>
2023-10-21 02:16:06 +1100

    Combined all three R together

--- public_transport.R
+++ public_transport.R
@@ -1,37 +1,20 @@
 #install library this project use
-list_packages <- c('ggplot2','shiny','dplyr','maps','leaflet','ggthemes','shinydashboard','scales','shinyWidgets','plotly',
-                   'sf', 'readr', 'rnaturalearth', 'rnaturalearthdata','scales','shinyjs',"httr","jsonlite","png","grid","googleway","shinyWidgets")
+list_packages <- c(
+  'ggplot2', 'shiny', 'dplyr', 'maps', 'leaflet', 'ggthemes', 'shinydashboard', 
+  'scales', 'shinyWidgets', 'plotly', 'sf', 'readr', 'shinyjs', "httr", "jsonlite", "png", 
+  "grid", "googleway", 'ggiraph', 'tidyr'
+)
 new_packages <- list_packages[!list_packages %in% installed.packages()[,"Package"]]
 if(length(new_packages)) install.packages(new_packages)
 
-library("shinyWidgets")
-library("httr")
-library("jsonlite")
-library("png")
-library("grid")
-library("shiny")
-library("dplyr")
-library("maps")
-library("leaflet")
-library("ggplot2")
-library("shiny")
-library("ggthemes")
-library("shinydashboard")
-library("scales")
-library("shinyWidgets")
-library("plotly")
-library("sf")
-library("readr")
-library("rnaturalearth")
-library("scales")
-library('shinyjs')
-library("googleway")
+lapply(list_packages, library, character.only = TRUE)
 source('tableau-in-shiny-v1.0.R')
 
 ##################
 # DATA   PROCESS #
 ##################
 mode_patronage <- read.csv("dataset/patronage_by_transport_mode.csv", stringsAsFactors = FALSE) 
+bike_data <- read.csv("dataset/bicycle.csv")
 #only remain the data of weekday because our topic is commute to CBD
 weekday_mode_patronage <- mode_patronage %>% filter(Day_type == "Normal Weekday") %>% group_by(Day_of_week) %>% summarise(Patronage = sum(Pax_daily))
 weekday_mode_patronage$Day_of_week <- factor(weekday_mode_patronage$Day_of_week, 
@@ -162,7 +145,61 @@ station_map <- tabPanel(
   cellWidths = c("50%","50%"))
 )
 
-#combine two pages above
+#page of car traffic volumn
+volumn_tab <-tabPanel("Traffic Volumn", 
+                      fluidPage(
+                        sidebarLayout(
+                          sidebarPanel(
+                            sliderInput(inputId = "dayOfWeek", 
+                                        label = "Day Of Week", 
+                                        min = 1, max = 7, value = c(1, 7), step = 1),
+                            
+                            checkboxGroupInput(inputId = "bound", 
+                                               label = "Bound", 
+                                               choices = c("EAST BOUND", "NORTH BOUND", 
+                                                           "NORTH EAST BOUND", "NORTH WEST BOUND", 
+                                                           "SOUTH BOUND", "SOUTH EAST BOUND", 
+                                                           "SOUTH WEST BOUND", "WEST BOUND"),
+                                               selected = c("EAST BOUND", "NORTH BOUND", 
+                                                            "NORTH EAST BOUND", "NORTH WEST BOUND", 
+                                                            "SOUTH BOUND", "SOUTH EAST BOUND", 
+                                                            "SOUTH WEST BOUND", "WEST BOUND"))
+                          ),
+                          mainPanel(
+                            tableauPublicViz(     
+                              id='volumn_tableau',       
+                              url = 'https://public.tableau.com/views/volumn_draft3/map1?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link',
+                              height="1000px"
+                            ),
+                          )
+                        )
+                      )
+)
+#page of car parking area
+parking_tab <- tabPanel("Parking Area", 
+                        fluidPage(
+                          sidebarLayout(
+                            sidebarPanel(
+                              checkboxGroupInput(
+                                inputId = "parkingType", 
+                                label = "Parking type", 
+                                choices = c("Commercial", "Private", "Residential"),
+                                selected = c("Commercial", "Private", "Residential")
+                              )
+                            ),
+                            mainPanel(
+                              tableauPublicViz(
+                                id='parking_tableau',
+                                url = 'https://public.tableau.com/views/ParkingAreainMelbourne0/1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link',
+                                height="1000px",
+                                width="1000px"
+                              )
+                            )
+                          )
+                        )
+)
+
+#combine all page above
 public_transport <- tabPanel(
   title='Public Transport',
   sidebarLayout(
@@ -196,28 +233,43 @@ public_transport <- tabPanel(
   )
 )
 
-Taxi_uber <- tabPanel(
-  title='Drive'
+bike <- tabPanel(
+  title='Bike Transport',
+  h2('Bicycle route in Melbourne'),
+  splitLayout(
+    girafeOutput('plot_bike'),
+    tableauPublicViz(
+      id="tableauviz",
+      url='https://public.tableau.com/views/Bicycle_16972615762500/BicyclerouteinMelbourne?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link',
+      height='500px',
+      width='800px'
+    ),
+    girafeOutput('plot_new')
+  )
 )
 
-Bicycle<- tabPanel(
-  title='Bicycle'
+driver <- tabPanel(
+  title='Car Transport',
+  tabsetPanel(
+    volumn_tab,parking_tab)
 )
+
+
 ui <- navbarPage(
   header=setUpTableauInShiny(),
   #use bootstrap
   theme = bslib::bs_theme(bootswatch = "quartz"),
   title='Commute to CBD',
   public_transport,
-  Taxi_uber,
-  Bicycle
+  bike,
+  driver
 )
 
 ################
 # SHINY SERVER #
 ################
 server <- function(input, output, session) {
-  
+# public transport server  
   #station selected by user
   station_selected <-reactiveVal(NULL)
   
@@ -345,8 +397,105 @@ server <- function(input, output, session) {
   output$location_near_station <- renderTable({
     location_data() 
   })
+
+# bike transport server
+  output$plot_bike <- renderGirafe({
+    
+    # Filter the data and count the number of sightings in each state
+    bike_counts <- bike_data %>%
+      group_by(name) %>%
+      summarize(Count = n())
+    
+    # ggplot graph for Statistics
+    p <- ggplot(bike_counts) + aes(x = name, y = Count, tooltip = paste("Name: ", name, "\nCount: ", Count), data_id=name) +
+      geom_bar_interactive(stat = 'identity', width = 0.8, fill = '#8f00b6') +
+      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
+      labs(x = 'Bicycle Route type', y = 'Count') +
+      theme_minimal() +
+      ggtitle(paste("Bicycle Type Statistics"))+
+      theme(panel.background=element_blank(),
+            panel.grid.major.y=element_line(color='#e2e2e2'),
+            axis.ticks=element_blank(),
+            axis.text.x = element_text(angle = 90, hjust = 1))
+    
+    girafe(ggobj=p, height_svg=8, width_svg = 12)
+    
+  })
   
   
+  #javascript part
+  observeEvent(input$plot_bike_selected, {
+    # Clear selection from bar chart
+    session$sendCustomMessage(type='plot_bike_set', message=character(0))
+    
+    # Filter Tableau map by the selected bike route
+    name <- input$plot_bike_selected
+    runjs(sprintf('let viz = document.getElementById("tableauviz");
+        let sheet = viz.workbook.activeSheet;
+        sheet.applyFilterAsync("Name", ["%s"], FilterUpdateType.Replace);', name))
+  })
+  
+  
+  #tableau 
+  output$plot_new <- renderGirafe({
+    bike_tableau_name <- input$tableauviz_mark_selection_changed$Name[1]
+    if(is.null(bike_tableau_name)) return()
+    # Filter the data by the selected bike route
+    filtered_data <- bike_data %>% 
+      filter(name == bike_tableau_name)
+    
+    direction_counts <- filtered_data %>%
+      group_by(direction) %>%
+      summarize(Count = n())
+    
+    p <- ggplot(direction_counts) + aes(x = direction, y = Count, tooltip = paste("Direction: ", direction, "\nCount: ", Count), data_id=direction) +
+      geom_bar_interactive(stat = 'identity', width = 0.8, fill = '#8f00b6') +
+      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
+      labs(x = 'Direction', y = 'Count') +
+      theme_minimal() +
+      ggtitle(paste("Direction statistics"))+
+      theme(axis.text.x = element_text(hjust = 1))
+    girafe(ggobj=p, height_svg= 4)
+  })
+
+# car transport server
+  ## Volumn - Slide for Day of Week 
+  observeEvent(input$dayOfWeek, {
+    min_val <- input$dayOfWeek[1]
+    max_val <- input$dayOfWeek[2]
+    runjs(sprintf('let viz = document.getElementById("volumn_tableau");
+                   let sheet = viz.workbook.activeSheet;
+                   sheet.applyRangeFilterAsync("Day Of Week", { min: %d, max: %d }, FilterUpdateType.REPLACE);', 
+                  min_val, max_val))
+  })
+  
+  
+  ## Volumn - Bound's Check Box
+  observeEvent(input$bound, {
+    bound_selections <- input$bound
+    bound_str <- paste(bound_selections, collapse = ", ")
+    cat("Selected bound types:", bound_str, "\n")
+    
+    runjs(sprintf('let viz = document.getElementById("volumn_tableau");
+        let sheet = viz.workbook.activeSheet;
+        let boundTypesArray = "%s".split(", ").map(s => s.trim());
+        sheet.applyFilterAsync("Bound", boundTypesArray, FilterUpdateType.Replace);', bound_str))
+  })
+  
+  
+  ## Parking - Parking type's Check Box
+  observeEvent(input$parkingType, { ##listen to parkingType checkbox， once user change checkbox， it will receive the values
+    parking_selections <- input$parkingType
+    parking_str <- paste(parking_selections, collapse = ", ")
+    cat("Selected parking types:", parking_str, "\n")
+    
+    runjs(sprintf('let viz = document.getElementById("parking_tableau");
+        let sheet = viz.workbook.activeSheet;
+        let parkingTypesArray = "%s".split(", ").map(s => s.trim());
+        sheet.applyFilterAsync("Parking type", parkingTypesArray, FilterUpdateType.Replace);', parking_str))
+  })
+  
+
 }
 
 
