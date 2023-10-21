8bcad3419fb296aea29e5652a08a8973ffd9a67f
vitasoy_killer <1335289380@qq.com>
2023-10-21 21:40:13 +1100

    final version

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # IV_assignment3
 
-### Public transport
+| Group member | Student ID |
+| ------------ | ---------- |
+| Yuling Mao   | 1379392    |
+| Fan Yang     | 1387007    |
+| Zihao Chen   | 1454390    |
+| Zeyu Li      | 1397279    |
 
+### How To Run
+
+All the code is in 'app.R', so just open 'app.R' in R Studio and click 'Run App' to execute.
+
+### File Structure
+
+```
+.
+├── README.md 
+├── app.R #our code
+├── dataset #data used
+├── tableau-in-shiny-v1.0.R
+└── www #image
+    ├── logo-simple.svg
+    └── myki.png
+
+```
--- app.R
+++ app.R
@@ -1,37 +1,20 @@
-#install library this project use
-list_packages <- c('ggplot2','shiny','dplyr','maps','leaflet','ggthemes','shinydashboard','scales','shinyWidgets','plotly',
-                   'sf', 'readr', 'rnaturalearth', 'rnaturalearthdata','scales','shinyjs',"httr","jsonlite","png","grid","googleway","shinyWidgets")
-new_packages <- list_packages[!list_packages %in% installed.packages()[,"Package"]]
-if(length(new_packages)) install.packages(new_packages)
+# #install library this project use
+list_packages <- c(
+  'ggplot2', 'shiny', 'dplyr', 'maps', 'leaflet', 'ggthemes', 'shinydashboard',
+  'scales', 'shinyWidgets', 'plotly', 'sf', 'readr', 'shinyjs', "httr", "jsonlite", "png",
+  "grid", "googleway", 'ggiraph', 'tidyr'
+)
+# new_packages <- list_packages[!list_packages %in% installed.packages()[,"Package"]]
+# if(length(new_packages)) install.packages(new_packages)
 
-library("shinyWidgets")
-library("httr")
-library("jsonlite")
-library("png")
-library("grid")
-library("shiny")
-library("dplyr")
-library("maps")
-library("leaflet")
-library("ggplot2")
-library("shiny")
-library("ggthemes")
-library("shinydashboard")
-library("scales")
-library("shinyWidgets")
-library("plotly")
-library("sf")
-library("readr")
-library("rnaturalearth")
-library("scales")
-library('shinyjs')
-library("googleway")
+lapply(list_packages, library, character.only = TRUE)
 source('tableau-in-shiny-v1.0.R')
 
 ##################
 # DATA   PROCESS #
 ##################
 mode_patronage <- read.csv("dataset/patronage_by_transport_mode.csv", stringsAsFactors = FALSE) 
+bike_data <- read.csv("dataset/bicycle.csv")
 #only remain the data of weekday because our topic is commute to CBD
 weekday_mode_patronage <- mode_patronage %>% filter(Day_type == "Normal Weekday") %>% group_by(Day_of_week) %>% summarise(Patronage = sum(Pax_daily))
 weekday_mode_patronage$Day_of_week <- factor(weekday_mode_patronage$Day_of_week, 
@@ -40,12 +23,11 @@ weekday_mode_patronage$Day_of_week <- factor(weekday_mode_patronage$Day_of_week,
                                              ordered = TRUE)
 
 #read in a csv file about station location and patronage of each station  
-station_data <- read.csv("dataset/station_information.csv", stringsAsFactors = FALSE) 
+station_data <- read.csv("dataset/station_information.csv", stringsAsFactors = FALSE)  %>%
+                arrange(desc(Pax_norm_weekday))
 
 #get the most crowd station
-crowd_station <- station_data %>%
-  select(Stop_name, Pax_norm_weekday) %>%  # Select relevant columns
-  arrange(desc(Pax_norm_weekday)) %>%      # Sort by pax_norm_weekday in descending order
+crowd_station <- station_data %>%      # Sort by pax_norm_weekday in descending order
   slice_head(n = 10) 
 
 #############
@@ -84,14 +66,20 @@ icon_code <- get_weather_by_coordinates(city,api_key)$weather$icon[1]
 icon_url <- display_weather_icon(icon_code)
 
 #check whether returned
-print(get_weather_by_coordinates(city,api_key))
+
 weather_information <- get_weather_by_coordinates(city,api_key)
 
 #the key of Google map api
 map_api = "AIzaSyCypkxh4Yz5DOGFBt-1w9PPJI6h7v5-lfg"
 
 
-
+res <- google_places(location = c(-38.37423454,145.2218375),
+              place_type = "restaurants",
+              radius = 200,
+              key = map_api)$results
+print(names(res))
+res <- res %>% select(types,name)
+print(res)
 ##################
 # USER INTERFACE #
 ##################
@@ -102,24 +90,116 @@ route_map <- tabPanel(
   verticalLayout(
     #tableau map
     tableauPublicViz(
-      id='tableauViz',       
+      id='tableau_route_map',       
       url='https://public.tableau.com/views/Book1_16966699247180/Sheet1?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link',
       width = "100%",
       height = "40%"
     ),
     #bar chart of route patronage
-    plotlyOutput("route_patronage_bar_chart",width = "400px")
+    splitLayout(
+    plotlyOutput("route_patronage_bar_chart",width = "400px"),
+    div(
+      h2("Check Timetable Here"),
+      a(href = "https://www.ptv.vic.gov.au/timetables",
+        target = "_blank",
+        img(src = "logo-simple.svg", width = "20%")),
+      h2("Top Up Your Myki"),
+      a(href = "https://www.ptv.vic.gov.au/tickets/myki/?misdirected=1#topup",
+        target = "_blank",
+        img(src = "myki.png", width = "20%")),
+    ),
+      cellWidths = c("50%", "50%") 
+    )
+    )
   )
-)
 
 #page of station
 station_map <- tabPanel(
   title = "Map of Station",
-  google_mapOutput(outputId = "stop_map"),
-  plotlyOutput("station_bar_chart")
+  #select a staion
+  div(
+    selectInput(
+      "stop_select","Stop Selection:",
+      choices = unique(station_data$Stop_name)
+    ),
+  verticalLayout(
+    #station map
+  tableauPublicViz(
+    id='tableau_station_map',       
+    url='https://public.tableau.com/views/Book2_16974540932260/Sheet1?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link',
+    width = "100%",
+    height = "40%"
+  ),
+  splitLayout(
+    div(
+    h2("About This Station"),
+    #show patronage
+    valueBoxOutput("station_patronage"),
+    h3("Search Nearby"),
+    #search locations nearby station
+    textInput("location_type", "Enter location:", value = "Restaurants"),
+    actionButton("location_search_btn", "Search")),
+    tableOutput("location_near_station"),
+    cellWidths = c("30%","70%"))
+  ),
+  cellWidths = c("50%","50%"))
 )
 
-#combine two pages above
+#page of car traffic volumn
+volumn_tab <-tabPanel("Traffic Volumn", 
+                      fluidPage(
+                        sidebarLayout(
+                          sidebarPanel(
+                            sliderInput(inputId = "dayOfWeek", 
+                                        label = "Day Of Week", 
+                                        min = 1, max = 7, value = c(1, 7), step = 1),
+                            
+                            checkboxGroupInput(inputId = "bound", 
+                                               label = "Bound", 
+                                               choices = c("EAST BOUND", "NORTH BOUND", 
+                                                           "NORTH EAST BOUND", "NORTH WEST BOUND", 
+                                                           "SOUTH BOUND", "SOUTH EAST BOUND", 
+                                                           "SOUTH WEST BOUND", "WEST BOUND"),
+                                               selected = c("EAST BOUND", "NORTH BOUND", 
+                                                            "NORTH EAST BOUND", "NORTH WEST BOUND", 
+                                                            "SOUTH BOUND", "SOUTH EAST BOUND", 
+                                                            "SOUTH WEST BOUND", "WEST BOUND"))
+                          ),
+                          mainPanel(
+                            tableauPublicViz(     
+                              id='volumn_tableau',       
+                              url = 'https://public.tableau.com/views/volumn_draft3/map1?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link',
+                              height="1000px"
+                            ),
+                          )
+                        )
+                      )
+)
+#page of car parking area
+parking_tab <- tabPanel("Parking Area", 
+                        fluidPage(
+                          sidebarLayout(
+                            sidebarPanel(
+                              checkboxGroupInput(
+                                inputId = "parkingType", 
+                                label = "Parking type", 
+                                choices = c("Commercial", "Private", "Residential"),
+                                selected = c("Commercial", "Private", "Residential")
+                              )
+                            ),
+                            mainPanel(
+                              tableauPublicViz(
+                                id='parking_tableau',
+                                url = 'https://public.tableau.com/views/ParkingAreainMelbourne0/1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link',
+                                height="1000px",
+                                width="1000px"
+                              )
+                            )
+                          )
+                        )
+)
+
+#combine all page above
 public_transport <- tabPanel(
   title='Public Transport',
   sidebarLayout(
@@ -143,7 +223,7 @@ public_transport <- tabPanel(
       span(h2("Where Are You Going")),
       textInput("origin", "Enter starting point:", value = "Y Suites on A Beckket Street"),
       textInput("destination", "Enter destination:", value = "University of Melbourne"),
-      actionButton("search_btn", "Search")
+      actionButton("direction_search_btn", "Search")
     ),
     mainPanel(
       tabsetPanel(
@@ -153,32 +233,61 @@ public_transport <- tabPanel(
   )
 )
 
-Taxi_uber <- tabPanel(
-  title='Drive'
+bike <- tabPanel(
+  title='Bike Transport',
+  h2('Bicycle route in Melbourne'),
+  splitLayout(
+    girafeOutput('plot_bike'),
+    tableauPublicViz(
+      id="tableauviz",
+      url='https://public.tableau.com/views/Bicycle_16972615762500/BicyclerouteinMelbourne?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link',
+      height='500px',
+      width='800px'
+    ),
+    girafeOutput('plot_new')
+  )
 )
 
-Bicycle<- tabPanel(
-  title='Bicycle'
+driver <- tabPanel(
+  title='Car Transport',
+  tabsetPanel(
+    volumn_tab,parking_tab)
 )
+
+
 ui <- navbarPage(
   header=setUpTableauInShiny(),
   #use bootstrap
   theme = bslib::bs_theme(bootswatch = "quartz"),
   title='Commute to CBD',
   public_transport,
-  Taxi_uber,
-  Bicycle
+  bike,
+  driver
 )
 
 ################
 # SHINY SERVER #
 ################
 server <- function(input, output, session) {
-
+# public transport server  
+  #station selected by user
+  station_selected <-reactiveVal(NULL)
+  
+  #when user select a station, change the tableau map and filter the station data
+  observeEvent(input$stop_select, {
+    station_selected(station_data %>% filter(Stop_name == input$stop_select))
+    # Filter Tableau viz by the state that was clicked on the bar chart
+    station <- station_selected()$Stop_name
+    runjs(sprintf('let viz = document.getElementById("tableau_station_map");
+        let sheet = viz.workbook.activeSheet;
+        sheet.applyFilterAsync("Name", ["%s"], FilterUpdateType.Replace);', station))
+  }
+  )
+  
   #route patronage bar chart 
   output$route_patronage_bar_chart <- renderPlotly({
     #get changes of parameter in tableau map
-    transport_mode <- input$tableauViz_parameter_changed$value[1]
+    transport_mode <- input$tableau_route_map_parameter_changed$value[1]
     #print(transport_mode)
     if(is.null(transport_mode)){
       #the default selection of the transport mode is MetroBus
@@ -209,53 +318,57 @@ server <- function(input, output, session) {
             type='bar',
             marker = list(color = bar_colors)) %>%
       layout(title = list(text = paste("Daily Patronage of",transport_mode), font = list(color = "white")),
-             xaxis = list(title = "type of day", showgrid = FALSE, tickfont = list(color = "white"), titlefont = list(color = "white")),
+             xaxis = list(title = "", showgrid = FALSE, tickfont = list(color = "white"), titlefont = list(color = "white")),
              yaxis = list(title = "",showticklabels = FALSE, showgrid = FALSE, tickfont = list(color = "white")),
              paper_bgcolor = 'rgba(0,0,0,0)', plot_bgcolor = 'rgba(0,0,0,0)',
-             margin = list(t = 50, b = 50),
+             margin = list(t = 50, b = 50,r = 50),
              legend = list(font = list(color = "white")))
   })
   
+  #show weather 
   output$Weather_Today <- renderValueBox({
     valueBox(
       weather_information$weather$main[1], "Weather now"
     )
   })
   
+  #show temperature
   output$temp_Today <- renderValueBox({
     valueBox(
       paste(weather_information$main$temp, "°C"), "Temperature now"
     )
   })
-  
+  #show feels like temperature
   output$feel_temp_Today <- renderValueBox({
     valueBox(
       paste(weather_information$main$feels_like, "°C"), "Feels like"
     )
   })
+  
+  #show wind speed
   output$wind_speed <- renderValueBox({
     valueBox(
       paste(weather_information$wind$speed,"m/s"), "Wind speed now"
     )
   })
   
+  #show average patronage of selected station
+  output$station_patronage <- renderValueBox({
+    valueBox(
+      station_selected()$Pax_norm_weekday, "Average Patronage Weekday"
+    )
+  })
+  
+  #weather icon
   output$Weather_icon <- renderUI({
     tags$img(src = icon_url)
   })
   
-  output$stop_map <- renderGoogle_map({
-    #use google map API draw the station map
-    google_map(
-      key = map_api,
-      data = station_data,
-      zoom = 15
-    ) %>%
-      add_markers(lat = "Stop_lat", lon = "Stop_long")
-  })
-  
   directions_data <- reactiveVal(NULL)
   
-  observeEvent(input$search_btn, {
+  #when click the direction search button
+  observeEvent(input$direction_search_btn, {
+    #googleway function, which is used to find route between to location
     directions <- google_map_directions(
       origin = input$origin,
       destination = input$destination,
@@ -265,19 +378,124 @@ server <- function(input, output, session) {
     directions_data(directions)
   })
   
-  output$station_bar_chart <-renderPlotly({
-    plot_ly(data = crowd_station, 
-            x = ~Pax_norm_weekday, 
-            y = ~Stop_name, 
-            type = 'bar', 
-            marker = list(color = 'silver'))%>%
-      layout(title = "Daily Patronage",
-             xaxis = list(title = "type of day",showgrid = FALSE),
-             yaxis = list(title = NULL,showticklabels = FALSE,showgrid = FALSE),
-             paper_bgcolor = 'rgba(0,0,0,0)', plot_bgcolor = 'rgba(0,0,0,0)',
-             margin = list(t = 50, b = 50))
+  location_data <- reactiveVal(NULL)
+
+  #when click location search button
+  observeEvent(input$location_search_btn, {
+    #print(station_selected())
+    places <- google_places(
+      search_string = paste(input$location_type,station_selected()$Stop_name,"Melbourne",sep = ','),
+      radius = 1000,
+      key = map_api
+    )$results
+    places <- places[,c("name","business_status","user_ratings_total","rating")]
+    location_data(places)
+    }
+    )
+
+  #show location searched
+  output$location_near_station <- renderTable({
+    location_data() 
+  })
+
+# bike transport server
+  output$plot_bike <- renderGirafe({
+    
+    # Filter the data and count the number of sightings in each state
+    bike_counts <- bike_data %>%
+      group_by(name) %>%
+      summarize(Count = n())
+    
+    # ggplot graph for Statistics
+    p <- ggplot(bike_counts) + aes(x = name, y = Count, tooltip = paste("Name: ", name, "\nCount: ", Count), data_id=name) +
+      geom_bar_interactive(stat = 'identity', width = 0.8, fill = '#8f00b6') +
+      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
+      labs(x = 'Bicycle Route type', y = 'Count') +
+      theme_minimal() +
+      ggtitle(paste("Bicycle Type Statistics"))+
+      theme(panel.background=element_blank(),
+            panel.grid.major.y=element_line(color='#e2e2e2'),
+            axis.ticks=element_blank(),
+            axis.text.x = element_text(angle = 90, hjust = 1))
+    
+    girafe(ggobj=p, height_svg=8, width_svg = 12)
+    
   })
   
+  
+  #javascript part
+  observeEvent(input$plot_bike_selected, {
+    # Clear selection from bar chart
+    session$sendCustomMessage(type='plot_bike_set', message=character(0))
+    
+    # Filter Tableau map by the selected bike route
+    name <- input$plot_bike_selected
+    runjs(sprintf('let viz = document.getElementById("tableauviz");
+        let sheet = viz.workbook.activeSheet;
+        sheet.applyFilterAsync("Name", ["%s"], FilterUpdateType.Replace);', name))
+  })
+  
+  
+  #tableau 
+  output$plot_new <- renderGirafe({
+    bike_tableau_name <- input$tableauviz_mark_selection_changed$Name[1]
+    if(is.null(bike_tableau_name)) return()
+    # Filter the data by the selected bike route
+    filtered_data <- bike_data %>% 
+      filter(name == bike_tableau_name)
+    
+    direction_counts <- filtered_data %>%
+      group_by(direction) %>%
+      summarize(Count = n())
+    
+    p <- ggplot(direction_counts) + aes(x = direction, y = Count, tooltip = paste("Direction: ", direction, "\nCount: ", Count), data_id=direction) +
+      geom_bar_interactive(stat = 'identity', width = 0.8, fill = '#8f00b6') +
+      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
+      labs(x = 'Direction', y = 'Count') +
+      theme_minimal() +
+      ggtitle(paste("Direction statistics"))+
+      theme(axis.text.x = element_text(hjust = 1))
+    girafe(ggobj=p, height_svg= 4)
+  })
+
+# car transport server
+  ## Volumn - Slide for Day of Week 
+  observeEvent(input$dayOfWeek, {
+    min_val <- input$dayOfWeek[1]
+    max_val <- input$dayOfWeek[2]
+    runjs(sprintf('let viz = document.getElementById("volumn_tableau");
+                   let sheet = viz.workbook.activeSheet;
+                   sheet.applyRangeFilterAsync("Day Of Week", { min: %d, max: %d }, FilterUpdateType.REPLACE);', 
+                  min_val, max_val))
+  })
+  
+  
+  ## Volumn - Bound's Check Box
+  observeEvent(input$bound, {
+    bound_selections <- input$bound
+    bound_str <- paste(bound_selections, collapse = ", ")
+    cat("Selected bound types:", bound_str, "\n")
+    
+    runjs(sprintf('let viz = document.getElementById("volumn_tableau");
+        let sheet = viz.workbook.activeSheet;
+        let boundTypesArray = "%s".split(", ").map(s => s.trim());
+        sheet.applyFilterAsync("Bound", boundTypesArray, FilterUpdateType.Replace);', bound_str))
+  })
+  
+  
+  ## Parking - Parking type's Check Box
+  observeEvent(input$parkingType, { ##listen to parkingType checkbox， once user change checkbox， it will receive the values
+    parking_selections <- input$parkingType
+    parking_str <- paste(parking_selections, collapse = ", ")
+    cat("Selected parking types:", parking_str, "\n")
+    
+    runjs(sprintf('let viz = document.getElementById("parking_tableau");
+        let sheet = viz.workbook.activeSheet;
+        let parkingTypesArray = "%s".split(", ").map(s => s.trim());
+        sheet.applyFilterAsync("Parking type", parkingTypesArray, FilterUpdateType.Replace);', parking_str))
+  })
+  
+
 }
 
 
